fa22b373b5c2349ce8588a48a290392b4aa34698
John Thielman <john.thielman@gmail.com>
2019-09-16 17:58:43 -0500

    convert button works

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('Let\'s convert some temps!');
+
+const showOnPage = (divId, stringToShow) => {
+    document.getElementById(divId).innerHTML = stringToShow;
+}
+
+const convertTemp = () => {
+    const input = document.getElementById('numIn').value;
+    const celsius = document.getElementById('celsius');
+    if (celsius.checked) {
+        return Math.round(10 * ((input - 32) * (5/9))) / 10;
+    } else {
+        return Math.round(10 * (input * (9/5) + 32)) / 10;
+    }
+}
+
+const showTemp = () => {
+    showOnPage('converted', convertTemp());
+}
+
+const init = () => {
+    document.getElementById('big-button').addEventListener('click', showTemp);
+}
+
+init();
